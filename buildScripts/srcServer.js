60fc67f6218b32d20d5ac269ffdfcbbfe6cd1854
August Giles <augustgiles8@gmail.com>
2019-02-08 09:44:38 -0800

    added package.json and automation scripts

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,14 @@
+// this sets up app as an express development web server
+
+const express = require('express');
+const path = require('path');
+const open = require('open');
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
+
+app.listen(port, (err) => {
+  err ? console.log(err) : open(`http://localhost:${port}`);
+});
